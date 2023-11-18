c7746a78e9680dda86c9d12196d0c87aaf860d7c
Sourav Nanda <67404111+sourav-nanda@users.noreply.github.com>
2023-11-18 12:48:47 +0530

    Update termux-setup.sh

--- scripts/termux-setup.sh
+++ scripts/termux-setup.sh
@@ -13,12 +13,9 @@ pkg install git wget jq openssh python python-pip termux-api zsh -y
 #Install fish 
 pkg install git -y && git clone https://github.com/msn-05/termux-fish.git && cd termux-fish && chmod +x script.sh && ./script.sh
 
-
-
-
 #Fix for pip install failing to work
 pkg up
 pkg rei libexpat
 
 # Done
-echo '\nSetup is Done!'
+echo -e '\nSetup is Done!'
